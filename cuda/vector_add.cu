7adba4224a3eb397cc000f106d89a402eb93da60
Tamuka Manjemu <tkmanjemu@gmail.com>
2026-09-01 21:46:34 -0400

    chore: prepare CUDA backend

---
+++ cuda/vector_add.cu
@@ -0,0 +1,35 @@
+#include "vector_add.h"
+
+#include <cuda_runtime.h>
+
+__global__ void vector_add_kernel(
+    const float* a,
+    const float* b,
+    float* c,
+    int n
+) {
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < n) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void launch_vector_add(
+    const float* d_a,
+    const float* d_b,
+    float* d_c,
+    int n
+) {
+    constexpr int threads_per_block = 256;
+
+    const int blocks =
+        (n + threads_per_block - 1) / threads_per_block;
+
+    vector_add_kernel<<<blocks, threads_per_block>>>(
+        d_a,
+        d_b,
+        d_c,
+        n
+    );
+}
---
+++ cuda/vector_add.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+void launch_vector_add( const float* d_a, const float* d_b, float* d_c, int n );
